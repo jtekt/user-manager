4524c93f4c2fb5686ad71300142a3a5db7bd36b7
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-09 06:02:12 +0000

    serving front end separately

--- employee_manager.js
+++ employee_manager.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios')
 const neo4j = require('neo4j-driver').v1
-
 const secrets = require('./secrets');
 
 const app_port = 8097;
