6434830ecafe2a2cccc83bfc29d4d9e406c4a7d0
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-06 08:46:23 +0900

    password update route

--- routes/v2/auth.js
+++ routes/v2/auth.js
@@ -1,11 +1,18 @@
 const {Router} = require('express')
 const { login } = require('../../controllers/v2/auth.js')
-const { request_password_reset } = require('../../controllers/v2/password.js')
+const {
+  update_password,
+  request_password_reset,
+ } = require('../../controllers/v2/password.js')
 
 const router = Router()
 
 router.route('/login')
   .post(login)
+
+router.route('/password')
+  .patch(update_password)
+  .put(update_password)
 
 router.route('/password/reset')
   .post(request_password_reset)
