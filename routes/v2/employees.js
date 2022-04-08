55fb4fa44b8258680db1911604a526772153cc03
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-08 09:16:05 +0900

    disabled CUD on old api versions

--- routes/v2/employees.js
+++ routes/v2/employees.js
@@ -1,24 +1,24 @@
 const {Router} = require('express')
-const password_router = require('./password')
+// const password_router = require('./password')
 const {
+  // create_user,
   get_users,
-  create_user,
   get_user,
-  patch_user,
-  delete_user
+  // patch_user,
+  // delete_user
 } = require('../../controllers/v2/employee.js')
 
 const router = Router({mergeParams: true})
 
 router.route('/')
   .get(get_users)
-  .post(create_user)
+  // .post(create_user)
 
 router.route('/:user_id')
   .get(get_user)
-  .patch(patch_user)
-  .delete(delete_user)
+  // .patch(patch_user)
+  // .delete(delete_user)
 
-router.use('/:user_id/password', password_router)
+// router.use('/:user_id/password', password_router)
 
 module.exports = router
--- test/employees_v2.js
+++ test/employees_v2.js
@@ -68,137 +68,137 @@ describe("/v2/users", () => {
     })
   })
 
-  describe("POST /v2/users/", () => {
+  // describe("POST /v2/users/", () => {
+  //
+  //
+  //   it("Should allow user creation", async () => {
+  //
+  //     const {status, body} = await request(app)
+  //       .post("/v2/users/")
+  //       .send(new_user)
+  //       .set('Authorization', `Bearer ${admin_jwt}`)
+  //
+  //     if(body.properties) new_user_id = body.properties._id
+  //     expect(status).to.equal(200)
+  //   })
+  //
+  //   it("Should not allow unauthenticated user creation", async () => {
+  //     const {status} = await request(app)
+  //       .post("/v2/users/")
+  //       .send(new_user)
+  //     expect(status).to.equal(403)
+  //   })
+  // })
 
-
-    it("Should allow user creation", async () => {
-
-      const {status, body} = await request(app)
-        .post("/v2/users/")
-        .send(new_user)
-        .set('Authorization', `Bearer ${admin_jwt}`)
-
-      if(body.properties) new_user_id = body.properties._id
-      expect(status).to.equal(200)
-    })
-
-    it("Should not allow unauthenticated user creation", async () => {
-      const {status} = await request(app)
-        .post("/v2/users/")
-        .send(new_user)
-      expect(status).to.equal(403)
-    })
-  })
-
-  describe("POST /v2/auth/login", () => {
-
-
-    it("Should allow login as the new user", async () => {
-
-      const {status, body} = await request(app)
-        .post("/v2/auth/login")
-        .send(new_user)
-
-      new_user_jwt =body.jwt
-
-      expect(status).to.equal(200)
-    })
-
-    it("Should not allow unauthenticated user creation", async () => {
-      const {status} = await request(app)
-        .post("/v2/users/")
-        .send(new_user)
-      expect(status).to.equal(403)
-    })
-
-  })
-
-  describe("GET /v2/users/:user_id", () => {
-
-    it("Should not allow unauthenticated access to user", async () => {
-      const {status} = await request(app).get(`/v2/users/${user_id}`)
-      expect(status).to.equal(403)
-    })
-
-    it("Should allow authenticated users to query the created user", async () => {
-
-      const {status} = await request(app)
-        .get(`/v2/users/${new_user_id}`)
-        .set('Authorization', `Bearer ${admin_jwt}`)
-
-      expect(status).to.equal(200)
-    })
-  })
-
-  describe("PATCH /v2/users/:user_id/", () => {
-
-    it("Should prevent update of _id", async () => {
-      const {status} = await request(app)
-      .patch(`/v2/users/${new_user_id}/`)
-      .send({_id: 'banana'})
-      .set('Authorization', `Bearer ${admin_jwt}`)
-
-      expect(status).to.equal(403)
-    })
-
-    it("Should allow update own display name", async () => {
-      const {status} = await request(app)
-      .patch(`/v2/users/${new_user_id}/`)
-      .send({display_name: 'banana'})
-      .set('Authorization', `Bearer ${new_user_jwt}`)
-
-      expect(status).to.equal(200)
-    })
-
-    it("Should prevent update display name of others", async () => {
-      const {status} = await request(app)
-      .patch(`/v2/users/${admin_id}/`)
-      .send({display_name: 'banana'})
-      .set('Authorization', `Bearer ${new_user_jwt}`)
-
-      expect(status).to.equal(403)
-    })
-
-
-  })
-
-  describe("PUT /v2/users/:user_id/password", () => {
-
-    it("Should allow own password update", async () => {
-      const {status} = await request(app)
-      .put(`/v2/users/${new_user_id}/password`)
-      .send({new_password: 'newPassword', new_password_confirm: 'newPassword'})
-      .set('Authorization', `Bearer ${new_user_jwt}`)
-
-      expect(status).to.equal(200)
-    })
-
-    it("Should prevent password update of others", async () => {
-      const {status} = await request(app)
-      .put(`/v2/users/${admin_id}/password`)
-      .send({new_password: 'newPassword', new_password_confirm: 'newPassword'})
-      .set('Authorization', `Bearer ${new_user_jwt}`)
-
-      expect(status).to.equal(403)
-    })
-
-
-  })
-
-  describe("DELETE /v2/users/:user_id", () => {
-
-    it("Should not allow unauthenticated user deletion", async () => {
-      const {status} = await request(app).delete(`/v2/users/${user_id}`)
-      expect(status).to.equal(403)
-    })
-
-    it("Should allow user deletion", async () => {
-      const {status} = await request(app)
-        .delete(`/v2/users/${new_user_id}`)
-        .set('Authorization', `Bearer ${admin_jwt}`)
-      expect(status).to.equal(200)
-    })
-
-  })
+  // describe("POST /v2/auth/login", () => {
+  //
+  //
+  //   it("Should allow login as the new user", async () => {
+  //
+  //     const {status, body} = await request(app)
+  //       .post("/v2/auth/login")
+  //       .send(new_user)
+  //
+  //     new_user_jwt =body.jwt
+  //
+  //     expect(status).to.equal(200)
+  //   })
+  //
+  //   it("Should not allow unauthenticated user creation", async () => {
+  //     const {status} = await request(app)
+  //       .post("/v2/users/")
+  //       .send(new_user)
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  // })
+  //
+  // describe("GET /v2/users/:user_id", () => {
+  //
+  //   it("Should not allow unauthenticated access to user", async () => {
+  //     const {status} = await request(app).get(`/v2/users/${user_id}`)
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  //   it("Should allow authenticated users to query the created user", async () => {
+  //
+  //     const {status} = await request(app)
+  //       .get(`/v2/users/${new_user_id}`)
+  //       .set('Authorization', `Bearer ${admin_jwt}`)
+  //
+  //     expect(status).to.equal(200)
+  //   })
+  // })
+  //
+  // describe("PATCH /v2/users/:user_id/", () => {
+  //
+  //   it("Should prevent update of _id", async () => {
+  //     const {status} = await request(app)
+  //     .patch(`/v2/users/${new_user_id}/`)
+  //     .send({_id: 'banana'})
+  //     .set('Authorization', `Bearer ${admin_jwt}`)
+  //
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  //   it("Should allow update own display name", async () => {
+  //     const {status} = await request(app)
+  //     .patch(`/v2/users/${new_user_id}/`)
+  //     .send({display_name: 'banana'})
+  //     .set('Authorization', `Bearer ${new_user_jwt}`)
+  //
+  //     expect(status).to.equal(200)
+  //   })
+  //
+  //   it("Should prevent update display name of others", async () => {
+  //     const {status} = await request(app)
+  //     .patch(`/v2/users/${admin_id}/`)
+  //     .send({display_name: 'banana'})
+  //     .set('Authorization', `Bearer ${new_user_jwt}`)
+  //
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  //
+  // })
+  //
+  // describe("PUT /v2/users/:user_id/password", () => {
+  //
+  //   it("Should allow own password update", async () => {
+  //     const {status} = await request(app)
+  //     .put(`/v2/users/${new_user_id}/password`)
+  //     .send({new_password: 'newPassword', new_password_confirm: 'newPassword'})
+  //     .set('Authorization', `Bearer ${new_user_jwt}`)
+  //
+  //     expect(status).to.equal(200)
+  //   })
+  //
+  //   it("Should prevent password update of others", async () => {
+  //     const {status} = await request(app)
+  //     .put(`/v2/users/${admin_id}/password`)
+  //     .send({new_password: 'newPassword', new_password_confirm: 'newPassword'})
+  //     .set('Authorization', `Bearer ${new_user_jwt}`)
+  //
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  //
+  // })
+  //
+  // describe("DELETE /v2/users/:user_id", () => {
+  //
+  //   it("Should not allow unauthenticated user deletion", async () => {
+  //     const {status} = await request(app).delete(`/v2/users/${user_id}`)
+  //     expect(status).to.equal(403)
+  //   })
+  //
+  //   it("Should allow user deletion", async () => {
+  //     const {status} = await request(app)
+  //       .delete(`/v2/users/${new_user_id}`)
+  //       .set('Authorization', `Bearer ${admin_jwt}`)
+  //     expect(status).to.equal(200)
+  //   })
+  //
+  // })
 
 })
