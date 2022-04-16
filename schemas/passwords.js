170812fcb0920a112356a9961294a602b40751a7
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-16 21:49:18 +0900

    password regex

--- schemas/passwords.js
+++ schemas/passwords.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
 
   new_password: Joi.string()
       .min(6)
-      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+      .pattern(new RegExp(/^[ A-Za-z0-9_@./#&+-]*$/))
       .required(),
 
   new_password_confirm: Joi.ref('new_password'),
