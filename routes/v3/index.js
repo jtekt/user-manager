b7d60b77968d4830621fc5c245d5e1df1a3b4fe7
Maxime Moreillon <m.moreillon@gmail.com>
2022-07-22 09:53:42 +0900

    disabled group related routes

--- routes/v3/index.js
+++ routes/v3/index.js
@@ -7,7 +7,7 @@ router.use('/auth', require('./auth.js'))
 
 router.use('/employees', middleware, require('./employees.js'))
 router.use('/users', middleware, require('./employees.js'))
-router.use('/groups', middleware, require('./groups.js'))
+// router.use('/groups', middleware, require('./groups.js'))
 
 
 
