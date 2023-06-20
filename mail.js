ecbbac2aaab2daafa0662ca53c82c4ec811d1319
Maxime Moreillon <m.moreillon@gmail.com>
2023-06-20 21:42:50 +0900

    simplified email

--- mail.js
+++ mail.js
@@ -20,28 +20,19 @@ const options = {
 
 const transporter = nodemailer.createTransport(options)
 
-const send_email = (email) =>
-  new Promise((resolve, reject) => {
-    // This might already be a promise
-    transporter.sendMail(email, (error, info) => {
-      if (error) reject(error)
-      else resolve(info)
-    })
-  })
-
 exports.send_activation_email = async ({ url, user }) => {
   try {
     const { email_address } = user
     const token = await generate_token(user)
 
-    const activation_email = {
+    const email = {
       from: SMTP_FROM,
       to: email_address,
       subject: "Account activation",
       text: `Click the following link to register your account: ${url}/activate?token=${token}`,
     }
 
-    await send_email(activation_email)
+    await transporter.sendMail(email)
 
     console.log(`[Mail] Sent activation email to user ${email_address}`)
   } catch (e) {
@@ -61,7 +52,7 @@ exports.send_password_reset_email = async ({ url, user }) => {
       text: `Click the following link to reset your password: ${url}/password_update?token=${token}`,
     }
 
-    await send_email(email)
+    await transporter.sendMail(email)
 
     console.log(`[Mail] Sent password reset email to user ${email_address}`)
   } catch (e) {
