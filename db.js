9faaaa29d8ee56de3c9b15f0f31faed662b27a35
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-24 07:48:14 +0900

    refactoring for neo4j v5

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
-const neo4j = require('neo4j-driver')
-const dotenv = require('dotenv')
-const { hash_password } = require('./utils/passwords.js')
+const neo4j = require("neo4j-driver")
+const dotenv = require("dotenv")
+const { hash_password } = require("./utils/passwords.js")
 
 dotenv.config()
 
 const {
-  NEO4J_URL = 'bolt://neo4j',
-  NEO4J_USERNAME = 'neo4j',
-  NEO4J_PASSWORD = 'neo4j',
+  NEO4J_URL = "bolt://neo4j",
+  NEO4J_USERNAME = "neo4j",
+  NEO4J_PASSWORD = "neo4j",
 } = process.env
 
 const auth = neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
 
-
 const drivers = {
   v1: neo4j.driver(NEO4J_URL, auth, {}),
-  v2: neo4j.driver(NEO4J_URL, auth, { disableLosslessIntegers: true })
+  v2: neo4j.driver(NEO4J_URL, auth, { disableLosslessIntegers: true }),
 }
 
 const driver = drivers.v2 // alias
@@ -26,24 +25,21 @@ const get_connection_status = async () => {
   const session = driver.session()
   try {
     console.log(`[Neo4J] Testing connection...`)
-    await session.run('RETURN 1')
+    await session.run("RETURN 1")
     console.log(`[Neo4J] Connection successful`)
     return true
-  }
-  catch (e) {
+  } catch (e) {
     console.log(`[Neo4J] Connection failed`)
     return false
-  }
-  finally {
+  } finally {
     session.close()
   }
 }
 
-
 const set_ids_to_nodes_without_ids = async () => {
   const id_setting_query = `
   MATCH (u:User)
-  WHERE NOT EXISTS(u._id)
+  WHERE u._id IS NOT NULL
   SET u._id = toString(id(u))
   RETURN COUNT(u) as count
   `
@@ -52,30 +48,26 @@ const set_ids_to_nodes_without_ids = async () => {
 
   try {
     const { records } = await session.run(id_setting_query)
-    const count = records[0].get('count')
+    const count = records[0].get("count")
     console.log(`[Neo4J] ID of ${count} nodes have been set`)
-  }
-  catch (error) {
+  } catch (error) {
     console.error(`Setting IDs failed`)
     throw error
-  }
-  finally {
+  } finally {
     session.close()
   }
 }
 
 const create_admin_if_not_exists = async () => {
-
   console.log(`[Neo4J] Creating admin account`)
 
   const session = driver.session()
 
   try {
     const {
-      DEFAULT_ADMIN_USERNAME: admin_username = 'administrator',
-      DEFAULT_ADMIN_PASSWORD: admin_password = 'administrator',
+      DEFAULT_ADMIN_USERNAME: admin_username = "administrator",
+      DEFAULT_ADMIN_PASSWORD: admin_password = "administrator",
     } = process.env
-
 
     const password_hashed = await hash_password(admin_password)
 
@@ -86,7 +78,7 @@ const create_admin_if_not_exists = async () => {
       // Check if the administrator account is missing its password
       // If the administrator account does not have a password (newly created), set it
       WITH administrator
-      WHERE NOT EXISTS(administrator.password_hashed)
+      WHERE administrator.password_hashed IS NOT NULL
       SET administrator.password_hashed = $password_hashed
       SET administrator._id = randomUUID() // THIS IS IMPORTANT
       SET administrator.isAdmin = true
@@ -98,66 +90,56 @@ const create_admin_if_not_exists = async () => {
       RETURN administrator
       `
 
-    const { records } = await session.run(query, { admin_username, password_hashed })
+    const { records } = await session.run(query, {
+      admin_username,
+      password_hashed,
+    })
 
-    if (records.length) console.log(`[Neo4J] Admin creation: admin account created`)
+    if (records.length)
+      console.log(`[Neo4J] Admin creation: admin account created`)
     else console.log(`[Neo4J] Admin creation: admin already existed`)
-
-
-
   } catch (error) {
     console.error(`Admin creation failed`)
-    throw (error)
+    throw error
   } finally {
     session.close()
   }
 }
 
 const create_constraints = async () => {
-
   const session = driver.session()
 
   try {
-    await session.run(`CREATE CONSTRAINT ON (u:User) ASSERT u._id IS UNIQUE`)
-    await session.run(`CREATE CONSTRAINT ON (u:User) ASSERT u.email_address IS UNIQUE`)
+    await session.run(`CREATE CONSTRAINT FOR (u:User) REQUIRE u._id IS UNIQUE`)
+    await session.run(
+      `CREATE CONSTRAINT FOR (u:User) REQUIRE u.email_address IS UNIQUE`
+    )
     // await session.run(`CREATE CONSTRAINT ON (u:User) ASSERT u.username IS UNIQUE`)
     console.log(`[Neo4J] Created constraints`)
-  }
-  catch (error) {
+  } catch (error) {
     console.error(`Creating contraints failed`)
     throw error
-  }
-  finally {
+  } finally {
     session.close()
   }
-
 }
 
 const init = async () => {
-
-
-
   if (await get_connection_status()) {
     connected = true
 
     try {
-      console.log('[Neo4J] Initializing DB')
+      console.log("[Neo4J] Initializing DB")
       await create_admin_if_not_exists()
       await set_ids_to_nodes_without_ids()
       await create_constraints()
       console.error(`[Neo4J] DB initialized`)
-
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error)
     }
   } else {
     setTimeout(init, 10000)
   }
-
-
-
-
 }
 
 exports.url = NEO4J_URL
