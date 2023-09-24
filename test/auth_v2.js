bedef816293508f137136698ed3838aef6891ae6
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-25 07:36:56 +0900

    fixing tdd

--- test/auth_v2.js
+++ test/auth_v2.js
@@ -1,45 +1,38 @@
 const request = require("supertest")
-const {expect} = require("chai")
-const {app} = require("../index.js")
-const dotenv = require('dotenv')
+const { expect } = require("chai")
+const { app } = require("../index.js")
+const dotenv = require("dotenv")
 dotenv.config()
 
-const sleep = (delay) => new Promise(resolve => setTimeout(resolve,delay))
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-const {
-  TEST_USERNAME = 'administrator',
-  TEST_PASSWORD = 'administrator',
-} = process.env
+const { TEST_USERNAME = "administrator", TEST_PASSWORD = "administrator" } =
+  process.env
 
 // We will test for api users
 describe("/v2/auth", () => {
-
-  before( async () => {
+  before(async () => {
     //console.log = function () {}
-    await sleep(7000) // wait for admin account to create (DIRTY)
-
+    await sleep(8000) // wait for admin account to create (DIRTY)
   })
-
 
   // We will test root GET related logics
   describe("POST /v2/auth/login", () => {
-
     // What should it do
     it("Should allow login with correct credentials", async () => {
-      const {status} = await request(app)
+      const { status } = await request(app)
         .post("/v2/auth/login")
-        .send({username: TEST_USERNAME, password: TEST_PASSWORD})
+        .send({ username: TEST_USERNAME, password: TEST_PASSWORD })
 
       expect(status).to.equal(200)
     })
 
     it("Should not allow random user login", async () => {
-      const {status} = await request(app)
+      const { status } = await request(app)
         .post("/v2/auth/login")
-        .send({username: 'roger', password: 'banana'})
+        .send({ username: "roger", password: "banana" })
 
       expect(status).to.equal(403)
     })
   })
-
 })
