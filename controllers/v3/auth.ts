c81ca7c412278ff29d292f38d8f7bd22f508b713
Maxime Moreillon <m.moreillon@gmail.com>
2023-12-14 08:43:02 +0900

    fixing expiry

--- controllers/v3/auth.ts
+++ controllers/v3/auth.ts
@@ -146,7 +146,7 @@ export const middleware = async (
 
     if (JWT_EXPIRATION_TIME && JWT_EXPIRATION_TIME !== "infinite") {
       const now = new Date().getTime() / 1000
-      if (now - iat) throw `Token has expired`
+      if (now - iat > Number(JWT_EXPIRATION_TIME)) throw `Token has expired`
     }
 
     res.locals.user = user
